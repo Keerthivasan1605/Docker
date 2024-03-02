a891cf2988c5348774f7c883d599680828172522
Keerthivasan1605 <72063709+Keerthivasan1605@users.noreply.github.com>
2024-03-02 17:22:05 +0530

    Update settings.py

--- devops/devops/settings.py
+++ devops/devops/settings.py
@@ -16,8 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Default value for the 'PORT' setting
-PORT = 8000
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
